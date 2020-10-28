cbbeab453a45b97c02a6885a783eace79c26dfc3
Kwabang <kwabang2827@gmail.com>
2020-10-28 20:48:09 +0900

    Update checking API status

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ app.use((request, response, next) => {
 	next()
 })
 
-app.get('/', (response) => {
+app.get('/', (request, response) => {
 	response.json({
 		'message': "Welcome to Twitch API"
 	})
 })
 
-app.get('/hls', (response) => {
+app.get('/hls', (request, response) => {
 	response.json({
 		'message': 'unknown channel name'
 	})
@@ -22,7 +22,6 @@ app.get('/hls', (response) => {
 
 app.get('/hls/:id', async (request, response) => {
 	let id = request.params.id
-	try {
 		let token = await fetch(`https://api.twitch.tv/api/channels/${id}/access_token`, {
 			method: 'GET',
 			headers: {
@@ -30,27 +29,38 @@ app.get('/hls/:id', async (request, response) => {
 				'Content-Type': 'application/json'
 			}
 		})
-		if (await token.status == 404) {
-			response.json({
-				'message': 'unknown channel name'
-			})
-			return
+	
+		switch(await token.status) {
+			case 200: //Channel founded
+				raw = await token.json()
+				url = `http://usher.twitch.tv/api/channel/hls/${id}.m3u8?player=twitchweb&&token=${raw.token}&sig=${raw.sig}&allow_audio_only=true&allow_source=true&type=any&p=${parseInt(Math.random() * 999999)}`
+				let hls = await fetch(url, {
+					method: 'GET'
+				})
+				switch(await hls.status) {
+					case 200: //m3u8 data exist
+						hls = await hls.text()
+						hls = hls.replace(/.*#.*\n?/gm, '')
+						response.json(hls.split('\n'))
+						return
+					default: //m3u8 data doesn't exsit
+						response.json({
+							'message': 'm3u8 data not found'
+						})
+						return
+				}
+			case 404: //Channel not founded
+				response.json({
+					'message':'Channel not found'
+				})
+				return
+			default: //Error with connect with Twitch API
+				response.json({
+					'message':'Error with Twitch API'
+				})
+				return
 		}
-		raw = await token.json()
-		url = `http://usher.twitch.tv/api/channel/hls/${id}.m3u8?player=twitchweb&&token=${raw.token}&sig=${raw.sig}&allow_audio_only=true&allow_source=true&type=any&p=${parseInt(Math.random() * 999999)}`
-		let hls = await fetch(url, {
-			method: 'GET'
-		})
-		hls = await hls.text()
-		hls = hls.replace(/.*#.*\n?/gm,'')
-		response.json(hls.split('\n'))
-	} catch (e) {
-		console.log(e)
-		response.json({
-			'message': 'Error with Twitch API'
-		})
-	}
-})
+	})
 
 app.listen(8080, () => {
 	console.log("API started")
